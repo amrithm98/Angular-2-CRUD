8274b4592750226982c5b285a63594218f5f87a8
amrithm98 <amrithchallenger@gmail.com>
2017-04-08 12:35:44 +0530

    Working

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-var PORT = 5000 || process.env.PORT;
-var express = require('express');
-var DB = "mongodb://localhost:27017/angular2-query";
+var PORT = 8000 || process.env.PORT;
+var mainRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
+var DB = "mongodb://localhost/angularHuman";
 var mongoose = require('mongoose');
+var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var morgan = require('morgan');
-var mainRouter = require('./routes/index');
-var apiRouter = require('./routes/api');
 var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', mainRouter);
 app.use('/api', apiRouter);
 mongoose.connect(DB, function(err) {
-    if (err)
+    if (err) {
         return err;
-    else
-        console.log('Connected to DB');
+    } else {
+        console.log('Successfully connected to ' + DB);
+    }
 });
-app.set('views', path.join(__dirname, 'client/views'))
+app.set('views', __dirname + '/client/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'client')));
+
+
 app.listen(PORT, function() {
-    console.log('running');
+    console.log('Listening on port ' + PORT);
 });
